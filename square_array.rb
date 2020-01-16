dcac7ceb4d6d0308c02ff616f4c4bad727e017bb
Jesse Woolsey <jesse232@live.com>
2020-01-16 10:12:09 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,16 @@
+<<<<<<< HEAD
 def square_array(array)
    output = []
    array.each do |x|
        output << x ** 2
     end
   output
+=======
+def square_array(array)
+  numbers = [1,2,3]
+  square_array(numbers)
+  
+  new_numbers = [9,10,16,25]
+  square_array(new_numbers)
+>>>>>>> 78ada2ec46f1b022db3b68041a2f3e44aebbbaf4
 end
